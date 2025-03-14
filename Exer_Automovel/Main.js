306985d23eb3f8e7c77c55938866657eb0fe2cee
Mateus <mateuscf938@gmail.com>
2025-03-13 21:47:11 -0300

    Novo Exercício

---
+++ Exer_Automovel/Main.js
@@ -0,0 +1,45 @@
+const Pessoa = require('./Pessoa');
+
+class Main{
+    constructor(){
+        this.pessoa = []
+    }
+
+    addPessoa(Pessoa){
+        this.pessoa.push(Pessoa);
+    }
+    addAutomovelPessoa(codPessoa, codAuto, marca, modelo) {
+        let pessoa = this.pessoas.find(p => p.codigo === codPessoa);
+        if (pessoa) {
+            pessoa.inserirAutomovel(new Automovel(codAuto, marca, modelo));
+        }
+    }
+
+    transfereAutomovel(codPessoaOrigem, codPessoaDestino, codAutomovel) {
+        let origem = this.pessoas.find(p => p.codigo === codPessoaOrigem);
+        let destino = this.pessoas.find(p => p.codigo === codPessoaDestino);
+        if (origem && destino) {
+            let automovel = origem.getAutomovel(codAutomovel);
+            if (automovel) {
+                origem.removerAutomovelByCod(codAutomovel);
+                destino.inserirAutomovel(automovel);
+            }
+        }
+    }
+
+    mostrarTodasAsPessoas() {
+        this.pessoas.forEach(pessoa => console.log(pessoa.imprimirCompleto()));
+    }
+}
+
+const main = new Main();
+main.addPessoa(new Pessoa(1, "João"));
+main.addAutomovelPessoa(1, 1, "Fiat", "Uno");
+main.addAutomovelPessoa(1, 2, "Volkswagen", "Fusca");
+main.mostrarTodasAsPessoas();
+
+main.addPessoa(new Pessoa(2, "Maria"));
+main.transfereAutomovel(1, 2, 1);
+main.mostrarTodasAsPessoas();
+
+
---
+++ Exer_Automovel/Pessoa.js
@@ -0,0 +1,41 @@
+class Pessoa{
+    #codigo
+    #nome
+    #automovel
+
+    constructor(codigo, nome, automovel){
+        this.#codigo = codigo;
+        this.#nome = nome;
+        this.#automovel = [];
+    }
+
+    get codigo(){
+        return this.#codigo
+    }
+
+    get nome(){
+        return this.#nome
+    }
+
+    inserirAutomovel(automovel){
+        this.#automovel.push(automovel)
+    }
+    
+    removerAutomovelByCod(codigo) {
+        this.#automovel = this.#automovel.filter(auto => auto.codigo !== codigo);
+    }
+
+    getAutomovel(codigo) {
+        return this.#automovel.find(auto => auto.codigo === codigo) || null;
+    }
+
+    imprimir() {
+        return `Código: ${this.#codigo}, Nome: ${this.#nome}`;
+    }
+    imprimirCompleto() {
+        return `Código: ${this.#codigo}, Nome: ${this.#nome}\nAutomóveis: ${this.automoveis.length ? this.automoveis.map(a => `\n  ${a.imprimir()}`).join('') : "\n  Nenhum automóvel registrado."}`;
+    }
+    
+}
+
+module.exports = Pessoa;
